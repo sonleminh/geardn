78ddbb8e2ae33a13cfc8c4751d70ecf61f87fb3c
Son Le <sonlele2000@gmail.com>
2024-08-30 17:05:32 +0700

    add cssbaseline & login grid UI

--- src/app/(auth-layout)/login/page.tsx
+++ src/app/(auth-layout)/login/page.tsx
@@ -1,5 +1,24 @@
+import { Box, Grid2, Typography } from '@mui/material';
 import React from 'react';
 
 export default function Login() {
-  return <div>nin cai mo lon</div>;
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+        bgcolor: '#D7D6D9',
+      }}>
+      <Box sx={{ width: 1000, mx: 'auto', bgcolor: '#fff' }}>
+        <Grid2 container>
+          <Grid2 size={6}>cc</Grid2>
+          <Grid2 size={6}>
+            <Typography>Welcome back!</Typography>
+          </Grid2>
+        </Grid2>
+      </Box>
+    </Box>
+  );
 }
