ef81c5ab68ed7188c1b12e1ad0a5c523a3b5a470
Son Le <sonlele2000@gmail.com>
2026-01-07 16:27:24 +0700

    fix(client): comment product detail page to debug

--- geardn-client/src/app/(home)/[category]/[product]/page.tsx
+++ geardn-client/src/app/(home)/[category]/[product]/page.tsx
@@ -1,26 +1,27 @@
 export const revalidate = 60;
 
-import { Suspense } from "react";
+// import { Suspense } from "react";
 import { Box } from "@mui/material";
-import ProductDetailContainer from "./ProductDetailContainer";
-import ProductDetailSkeleton from "./ProductDetailSkeleton";
+// import ProductDetailContainer from "./ProductDetailContainer";
+// import ProductDetailSkeleton from "./ProductDetailSkeleton";
 
 // Lưu ý: Type params trong Next 15 là Promise
 export default async function ProductDetailPage() {
-//   {
-//   params,
-// }: {
-//   params: Promise<{ category: string; product: string }>;
-// }
+  //   {
+  //   params,
+  // }: {
+  //   params: Promise<{ category: string; product: string }>;
+  // }
   // const { product: productSlug } = await params;
 
   return (
     <Box sx={{ pb: 8 }}>
-      <Suspense fallback={<ProductDetailSkeleton />}>
+      cc
+      {/* <Suspense fallback={<ProductDetailSkeleton />}>
         <ProductDetailContainer
-        //  slug={productSlug}
+         slug={productSlug}
         />
-      </Suspense>
+      </Suspense> */}
     </Box>
   );
 }
