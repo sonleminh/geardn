9a2db144066ff8549600f8c84e8be223bda6c3c2
Son Le <sonlele2000@gmail.com>
2026-01-09 18:43:10 +0700

    fix(client): add unoptimized to SkeletonImage

--- geardn-client/src/components/common/SkeletonImage/index.tsx
+++ geardn-client/src/components/common/SkeletonImage/index.tsx
@@ -1,21 +1,21 @@
-'use client';
+"use client";
 
-import * as React from 'react';
-import Image, { ImageProps } from 'next/image';
-import { Box, Skeleton } from '@mui/material';
+import * as React from "react";
+import Image, { ImageProps } from "next/image";
+import { Box, Skeleton } from "@mui/material";
 
 const SkeletonImage: React.FC<ImageProps> = (props) => {
   const [isError, setIsError] = React.useState(false);
   const srcImg = props?.src;
   return (
-    <Box sx={{ position: 'relative', width: '100%', height: '100%' }}>
+    <Box sx={{ position: "relative", width: "100%", height: "100%" }}>
       {isError ? (
         <Skeleton
-          variant='rectangular'
-          animation='wave'
+          variant="rectangular"
+          animation="wave"
           sx={{
-            width: '100%',
-            height: '100%',
+            width: "100%",
+            height: "100%",
           }}
         />
       ) : (
@@ -26,13 +26,14 @@ const SkeletonImage: React.FC<ImageProps> = (props) => {
           fill
           sizes={
             props?.sizes ??
-            '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+            "(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           }
           onError={() => {
             setIsError(true);
           }}
           priority={props?.priority || false}
           quality={props?.quality || 75}
+          unoptimized
         />
       )}
     </Box>
