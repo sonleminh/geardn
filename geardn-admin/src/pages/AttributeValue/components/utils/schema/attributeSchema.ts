d9927b3ac6b004196ea105b589f695972d8ea0c3
Son Le <sonlele2000@gmail.com>
2026-02-27 19:13:26 +0700

    fix(admin): update schema validate to handle upsert attribute value

--- geardn-admin/src/pages/AttributeValue/components/utils/schema/attributeSchema.ts
+++ geardn-admin/src/pages/AttributeValue/components/utils/schema/attributeSchema.ts
@@ -1,11 +1,23 @@
-import * as yup from 'yup'
+import * as yup from "yup";
 
 export const createSchema = yup.object({
-    attributeId: yup.string().required('Nội dung này không được để trống!').max(50, 'Không được vượt quá 50 ký tự!')
-    , value: yup.string().required('Nội dung này không được để trống!').max(50, 'Không được vượt quá 50 ký tự!')
-})
+  attributeId: yup
+    .string()
+    .required("Nội dung này không được để trống!")
+    .max(50, "Không được vượt quá 50 ký tự!"),
+  value: yup
+    .string()
+    .required("Nội dung này không được để trống!")
+    .max(100, "Không được vượt quá 100 ký tự!"),
+});
 
 export const updateSchema = yup.object({
-  attributeId: yup.string().required('Nội dung này không được để trống!').max(50, 'Không được vượt quá 50 ký tự!')
-  , value: yup.string().required('Nội dung này không được để trống!').max(50, 'Không được vượt quá 50 ký tự!')
-})
+  attributeId: yup
+    .string()
+    .required("Nội dung này không được để trống!")
+    .max(50, "Không được vượt quá 50 ký tự!"),
+  value: yup
+    .string()
+    .required("Nội dung này không được để trống!")
+    .max(100, "Không được vượt quá 100 ký tự!"),
+});
